368722b486d68ac8ba63c9afadc33736b24cec1a
hugodeco <hugodeco@sufficit.com.br>
2022-03-01 15:07:08 +0000

    Deploying to gh-pages from  @ d5cadfafa5e7d4dc89404dc70be39c9aab3402b4 🚀

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+﻿console.debug("SUFFICIT: Loading from RequireJs");    
+requirejs.config({
+    baseUrl: '/assets/js',
+    paths: {
+        'sufficit/blazor-before': 'sufficit-blazor-before',
+        'framework': '/_framework/blazor.webassembly',        
+        'content/mudblazor': '/_content/MudBlazor/MudBlazor.min',
+        'content/authentication': '/_content/Microsoft.AspNetCore.Components.WebAssembly.Authentication/AuthenticationService'
+    },
+    shim: {
+        'content/authentication': { init: onAuthenticationLoaded },
+        'framework': {
+            deps: ['content/authentication'], init: onBlazorFrameworkLoaded
+        }
+    }
+});
+
+require(['sufficit/blazor-before', 'framework']);
+
+/** Carregando sistema de autenticação */
+function onAuthenticationLoaded() {
+    console.debug('SUFFICIT: Authentication system loaded');
+}
+
+/** Carregando framework principal, Blazor */
+function onBlazorFrameworkLoaded() {
+    console.debug("SUFFICIT: Blazor Framework loaded");
+}
