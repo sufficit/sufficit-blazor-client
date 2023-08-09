748f551133ee4eeecba6c4821aa5fb0cb7cf6531
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2023-08-09 18:19:31 -0300

    publishing

---
+++ published/wwwroot/_content/Sufficit.Telephony.JsSIP/jssip-service.js
@@ -0,0 +1,276 @@
+﻿export var WebPhone;
+export var DotNetObjectReference;
+export var AccountReference;
+
+let mediaSelfElementID = 'media-player-self';
+let audioRemoteElementID = 'audio-player-remote';
+let videoRemoteElementID = 'media-player-remote';
+
+/** Recupera o estado atual do serviço  */
+export const GetStatus = function() {
+    return WebPhone.status;
+}
+
+/**
+ * Saving dotnet object reference for service
+ * @param {any} dotNetObjectRef
+ */
+export const Reference = async function(jssip, dotNetObjectRef, accountRef) {
+    DotNetObjectReference = dotNetObjectRef;
+    AccountReference = accountRef;
+    if (!window.JsSIP) {
+
+        const el = document.getElementById('jssip');
+        if (!el) {
+            window.JsSIPLoading = await new Promise(resolve => {
+                console.debug('resolving: {0}', jssip);
+                CreateScriptTag(jssip, () => resolve(window.JsSIP), document.body);
+            });
+        } else {
+            await window.JsSIPLoading;
+        }
+    }
+
+    await dotNetObjectRef.invokeMethodAsync('onDependenciesLoaded', window.JsSIP);
+}
+
+export const CreateScriptTag = function(url, implementationCode, location) {
+    //url is URL of external file, implementationCode is the code
+    //to be called from the file, location is the location to 
+    //insert the <script> element
+
+    var scriptTag = document.createElement('script');
+    scriptTag.id = 'jssip'; // avoid duplicate tag
+    scriptTag.src = url;
+
+    scriptTag.onload = implementationCode;
+    scriptTag.onreadystatechange = implementationCode;
+
+    location.appendChild(scriptTag);
+}
+
+/**
+ * Ocorre assim que o arquivo base JsSIP for carregado completamente
+ * @param {any} config Configurações passadas pelo backend (JsSIPConfiguration)
+ */
+export const onJsSIPLoaded = function(config) {
+
+    // Criando sockets apartir dos textos passados
+    let sockets = [];
+    config.sockets.forEach(function (address) {
+        let socket = new JsSIP.WebSocketInterface(address);
+        sockets.push(socket);
+    });
+
+    // Atualizando o vetor com os sockets criados
+    config.sockets = sockets;
+
+    // Criando softphone padrão
+    WebPhone = new JsSIP.UA(config);
+
+    // Exposing WebPhone for Debug, insecure, dont be panic about that
+    window.WebPhone = WebPhone;
+
+    // Vinculando eventos
+    WebPhone.on('connected',            WPEvent.bind(this, 'onConnected'));
+    WebPhone.on('disconnected',         WPEvent.bind(this, 'onDisconnected'));
+    WebPhone.on('newMessage',           WPEvent.bind(this, 'onNewMessage'));
+    WebPhone.on('registered',           WPEvent.bind(this, 'NotifyRegistered'));
+    WebPhone.on('unregistered',         WPEvent.bind(this, 'NotifyUnregistered'));
+    WebPhone.on('registrationFailed',   WPEvent.bind(this, 'NotifyRegistrationFailed'));
+    WebPhone.on('ringing',              WPEvent.bind(this, 'onRinging'));
+    WebPhone.on('ack',                  WPEvent.bind(this, 'onAck'));    
+    WebPhone.on('newRTCSession',        WPEvent.bind(this, 'onNewRTCSession'));
+
+    // Inicializando
+    WebPhone.start();
+}
+
+/**
+ * Usada para filtrar o evento de nova seção
+ * Gera informação demais no JSON e da erro
+ * @param {any} mappedEvent
+ * @param {any} data
+ */
+const WPEvent = function(mappedEvent, data) {
+    switch (mappedEvent) {
+        case 'onNewRTCSession': {
+
+            data = data.session;
+
+            // include json converter
+            data.toJSON = JsSIPSessionToJson;
+            break;
+        }
+        case 'onDisconnected': {
+            console.debug(data);
+            break;
+        }
+        case 'NotifyRegistered': case 'NotifyUnregistered': case 'NotifyRegistrationFailed': {
+            AccountReference.invokeMethodAsync(mappedEvent, data);
+            return undefined;
+        }
+        default: break;
+    }
+
+    DotNetObjectReference.invokeMethodAsync(mappedEvent, data);
+}
+
+/** JSON Replacer for the sessions */
+const JsSIPSessionToJson = function() {
+    let properties = ['id', 'direction', 'status'];
+    var result = {};
+    for (const element of properties) {
+        result[element] = this[element];
+    }
+    return result;
+}
+
+export const TestDevices = async function(request) {
+    console.debug('testing devices request: {0}', request);
+    let success = false;
+    
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.mediaDevices.getUserMedia;
+    const method = new Promise(function (success, reject) {
+        navigator.getUserMedia(request, success, reject);
+    });
+
+    let message;
+    const response = await method.then(() => success = true).catch((ex) => message = `(${ex.code}) ${ex.name} => ${ex.message}`);
+    console.debug('response: {0}', response);
+
+    return {
+        request: request,
+        success: success,
+        message: message
+    };
+}
+
+
+export const MediaDeviceUpdate = async function(mediaKind, mediaDevice) {
+    console.debug(`MediaDeviceUpdate => ${mediaKind} :: ${mediaDevice}`);
+    switch (mediaKind) {
+        case 'audiooutput': {
+            AttachSinkId(mediaDevice); break;
+        }
+        case 'videoinput': {
+            let videoElement = document.getElementById(mediaSelfElementID);
+            if (!videoElement) {
+                videoElement = document.createElement('video');
+                videoElement.id = mediaSelfElementID;
+                document.body.appendChild(videoElement);
+            }
+
+            const mediaStream = await navigator.mediaDevices.getUserMedia({ video: { deviceId: { exact: mediaDevice } } });
+
+            if ('srcObject' in videoElement) {
+                videoElement.srcObject = mediaStream;
+            } else {
+                // Avoid using this in new browsers, as it is going away.
+                videoElement.src = URL.createObjectURL(mediaStream);
+            }
+            break;
+        }
+    }
+}
+
+/** Indica se o navegador suporte a escolha do dispositivo para saída de áudio */
+export function BrowserOutputSelectSupport() { return !('sinkId' in HTMLMediaElement.prototype); }
+
+/** Attach audio output device to video element using device/sink ID. */
+export const AttachSinkId = function(sinkId) {
+    const element = document.getElementById(audioRemoteElementID);
+    if (typeof element.sinkId !== 'undefined') {
+        element.setSinkId(sinkId)
+            .then(() => {
+                console.log(`Success, audio output device attached: ${sinkId}`);
+            })
+            .catch(error => {
+                let errorMessage = error;
+                if (error.name === 'SecurityError') {
+                    errorMessage = `You need to use HTTPS for selecting audio output device: ${error}`;
+                }
+
+                console.error(errorMessage);
+                // Jump back to first output device in the list as it's the default.
+                audioOutputSelect.selectedIndex = 0;
+            });
+    } else {
+        console.warn('Browser does not support output device selection.');
+    }
+}
+
+/*
+const JsSIPTestVideo = async function () {
+    console.debug(navigator.mediaDevices);
+    console.debug(await navigator.mediaDevices.getSupportedConstraints());
+
+    Testtt();
+    return undefined;
+
+    //return;
+    const mediaStream = await navigator.mediaDevices.getUserMedia({ video: true });
+
+    //console.debug('JsSIPTestVideo: ', mediaStream);
+    let videoElement = document.getElementById(mediaSelfElementID);
+    if (!videoElement) {
+        videoElement = document.createElement('video');
+        videoElement.id = mediaSelfElementID;
+        document.body.appendChild(videoElement);
+    }
+
+    if ('srcObject' in videoElement) {
+        videoElement.srcObject = mediaStream;
+    } else {
+        // Avoid using this in new browsers, as it is going away.
+        videoElement.src = URL.createObjectURL(mediaStream);
+    }
+}
+
+const Testtt = async function() {
+    const devices = await MediaDevices();
+
+    let cameraDevice = undefined;
+    devices.forEach(function (dev) {
+        if (!cameraDevice && dev.kind === 'videoinput') {
+            cameraDevice = dev;
+        }
+    });
+
+    if (cameraDevice) {
+        let videoElement = document.getElementById(mediaSelfElementID);
+        if (!videoElement) {
+            videoElement = document.createElement('video');
+            videoElement.id = mediaSelfElementID;
+            document.body.appendChild(videoElement);
+        }
+
+        const mediaStream = await navigator.mediaDevices.getUserMedia({ video: { deviceId: { exact: cameraDevice.deviceId } } });
+
+        if ('srcObject' in videoElement) {
+            videoElement.srcObject = mediaStream;
+        } else {
+            // Avoid using this in new browsers, as it is going away.
+            videoElement.src = URL.createObjectURL(mediaStream);
+        }
+    }
+}
+
+*/
+
+// Recupera a lista de dispositivos de mídia disponiveis no navegador (cameras e microfones)
+export const MediaDevices = async function() {
+    if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
+        console.warn('enumerateDevices() not supported.');
+        return undefined;
+    }
+
+    return await new Promise(function(resolve) {
+        // List cameras and microphones.
+        navigator.mediaDevices.enumerateDevices()
+            .then(resolve)
+            .catch(function (err) {
+                console.error(err.name + ': ' + err.message);
+            });
+    });
+}
---
+++ published/wwwroot/_content/Sufficit.Telephony.JsSIP/jssip-sessions.js
@@ -0,0 +1,221 @@
+﻿export var DotNetObjectReference;
+
+/**
+ * Saving dotnet object reference for service
+ * @param {any} dotNetObjectRef
+ */
+export function Reference(dotNetObjectRef) { DotNetObjectReference = dotNetObjectRef; }
+
+/**
+ * Usado para vincular os eventos a uma seção recem criada
+ * @param {any} JsSIPSession
+ */
+export function onJsSIPSession(JsSIPSession) {
+    console.debug('onJsSIPSession: ', JsSIPSession);
+    const session = WebPhone._sessions[JsSIPSession.id];    
+    
+    session.on('newDTMF', e => dispatchSessionEvent(session, 'newDTMF', e));
+    session.on('newInfo', e => dispatchSessionEvent(session, 'newInfo', e));
+
+    session.on('hold', e => dispatchSessionEvent(session, 'hold', e));
+    session.on('muted', e => dispatchSessionEvent(session, 'muted', e));
+    session.on('unhold', e => dispatchSessionEvent(session, 'unhold', e));
+    session.on('unmuted', e => dispatchSessionEvent(session, 'unmuted', e));
+
+    session.on('progress', e => dispatchSessionEvent(session, 'progress', e));
+    session.on('succeeded', e => dispatchSessionEvent(session, 'succeeded', e));
+    //session.on('failed', e => dispatchSessionEvent(session, 'failed', e));
+    session.on('ended', e => dispatchSessionEvent(session, 'ended', e));
+
+    session.on('confirmed', e => dispatchSessionEvent(session, 'confirmed', e));
+
+    // emitido ai iniciar uma tentativa de conexão de seção
+    session.on('connecting', e => dispatchSessionEvent(session, 'connecting', e));
+    session.on('accepted', e => dispatchSessionEvent(session, 'accepted', e));
+    session.on('peerconnection', e => dispatchSessionEvent(session, 'peerconnection', e));
+
+    //session.on('peerconnection:createofferfailed', e => dispatchSessionEvent(session, 'peerconnection:createofferfailed', e));
+}
+
+/**
+ * Usado para vincular os eventos a uma seção recem criada
+ */
+export const Monitor = async function(id, reference) {
+    console.debug('monitor session: {id}', id);
+    const session = WebPhone._sessions[id];
+    if (!session) {
+        throw new Error('session not found');
+    }
+
+    session.on('newDTMF', e => reference.invokeMethodAsync('OnNewDTMF', e));
+    session.on('newInfo', e => reference.invokeMethodAsync('OnNewInfo', e));
+
+    session.on('hold', e => reference.invokeMethodAsync('OnHold', e));
+    session.on('muted', e => reference.invokeMethodAsync('OnMuted', e));
+    session.on('unhold', e => reference.invokeMethodAsync('OnUnhold', e));
+    session.on('unmuted', e => reference.invokeMethodAsync('OnUnmuted', e));
+
+    session.on('progress', e => reference.invokeMethodAsync('OnProgress', e));
+    session.on('accepted', e => { reference.invokeMethodAsync('OnAccepted', e); console.debug('accepted: {status}', session.status); });
+    session.on('succeeded', e => reference.invokeMethodAsync('OnSucceeded', e));
+    session.on('failed', e => reference.invokeMethodAsync('OnFailed', e));
+    session.on('ended', e => { reference.invokeMethodAsync('OnEnded', e); console.debug('ended: ', session.status); });
+
+    session.on('confirmed', e => { reference.invokeMethodAsync('OnConfirmed', e); console.debug('confirmed: ', session.status); });
+
+    // emitido ai iniciar uma tentativa de conexão de seção
+    session.on('connecting', e => reference.invokeMethodAsync('OnConnecting', e));
+    session.on('peerconnection', e => reference.invokeMethodAsync('OnPeerConnection', e));    
+}
+
+export const GetSession = async function (id) {
+    return WebPhone._sessions[id];
+}
+
+
+function dispatchSessionEvent(session, title, e) {
+    
+    if (title === 'peerconnection' || title === 'connecting') {
+        // Events to Media Stream
+        if (session.connection)
+            session.connection.addEventListener('addstream', onAddstream);
+    }
+
+    // adjust event name
+    let eventTitle = `on${title.charAt(0).toUpperCase()}${title.slice(1)}`;
+
+    // include json converter
+    e.toJSON = JsSIPSessionEventToJson;
+
+    console.debug(`dispatchSessionEvent: ${eventTitle}`, session);
+    DotNetObjectReference.invokeMethodAsync(eventTitle, session, e);
+}
+
+/** JSON Replacer for the sessions events */
+function JsSIPSessionEventToJson() {
+    let properties = ['originator', 'cause'];
+    let result = {}
+    for (var x in this) {
+        if (properties.includes(x)) {
+            let key = x;
+            if (key.startsWith("_")) key = key.slice(1);
+            result[key] = this[x];
+        }
+    }
+    //console.debug("JsSIPSessionEventToJson: ", this, result);
+    return result;
+}
+
+function onAddstream(event) {
+    
+    const tracks = event.stream.getTracks();
+    console.debug('onAddstream, event, tracks: ', event, tracks);
+
+    tracks.forEach(eachMediaTrack);
+}
+
+let audioRemoteElementID = 'audio-player-remote';
+let videoRemoteElementID = 'media-player-remote';
+function eachMediaTrack(track, index, array) {
+    let htmlElement;
+    if (track.kind === 'audio') {
+        htmlElement = document.getElementById(audioRemoteElementID);
+        if (!htmlElement) {
+            htmlElement = document.createElement('audio');
+            htmlElement.id = audioRemoteElementID;
+            document.body.appendChild(htmlElement);
+        }
+    } else if (track.kind === 'video') {
+        htmlElement = document.getElementById(videoRemoteElementID);
+        if (!htmlElement) {
+            htmlElement = document.createElement('video');
+            htmlElement.id = videoRemoteElementID;
+            document.body.appendChild(htmlElement);
+        }
+    } else {
+        console.error('unknown media stream');
+        return;
+    }
+
+    const mediaStream = new MediaStream([ track ]);
+    if ('srcObject' in htmlElement) {
+        htmlElement.srcObject = mediaStream;
+    } else {
+        // Avoid using this in new browsers, as it is going away.
+        htmlElement.src = URL.createObjectURL(mediaStream);
+    }
+
+    htmlElement.play();
+}
+
+
+//#region SESSION METHODS
+
+/**
+ * Execute actions to sessions
+ * @param {any} info Session basic information
+ * @param {any} action 
+ */
+export function JsSIPSessionActions(info, action, args) {
+    //console.debug("JsSIPSessionActions: ", info, action);
+    const session = WebPhone._sessions[info.id];
+    //console.debug(`JsSIPSessionActions: ${action}: `, session, args);
+    switch (action) {
+        case 'answer':
+            session.answer(args); // atender
+            break;
+        case 'terminate':
+            session.terminate(); //rejeitar
+            break;
+        case 'mute':
+            session.mute(args); // mutar audio ou/e video
+            break;
+        case 'unmute':
+            session.unmute(args); // habilitar audio ou/e video
+            break;
+        case 'hold':
+            session.hold(); // colocar chamada em espera
+            break;
+        case 'unhold':
+            session.unhold(); // continuar chamada em espera
+            break;
+
+
+        case 'Papayas':
+            console.log('Mangoes and papayas are $2.79 a pound.');
+            // expected output: "Mangoes and papayas are $2.79 a pound."
+            break;
+        default:
+            console.log(`sorry, we are out of ${action}.`);
+            break;
+    }
+}
+
+/**
+ * Originate a call session
+ * @param {any} uri
+ * @param {any} args
+ */
+export const Originate = async function (uri, args) {
+    return WebPhone.call(uri, args);
+}
+
+/**
+ * Terminate a call session
+ */
+export const Terminate = function (sessionId) {
+    const session = WebPhone._sessions[sessionId];
+    session.terminate();
+}
+
+/**
+ * Answer a call session
+ * @param {any} sessionId
+ * @param {any} args
+ */
+export const Answer = async function (sessionId, args) {
+    const session = WebPhone._sessions[sessionId];
+    session.answer(args);
+}
+
+//#endregion
