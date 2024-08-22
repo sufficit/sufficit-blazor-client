87cfd4d22333a4e1b9c7595757f1b562cd18aacb
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2024-08-22 17:12:49 -0300

    telephony carries from endpoint

--- src/Pages/Telephony/DirectInwardDialing/Object.razor.cs
+++ src/Pages/Telephony/DirectInwardDialing/Object.razor.cs
@@ -21,7 +21,7 @@ namespace Sufficit.Blazor.Client.Pages.Telephony.DirectInwardDialing
     [Authorize(Roles = "telephony")]
     public partial class Object : TelephonyBasePageComponent, IDisposable, IPage
     {
-        public const string RouteParameter = "pages/telephony/did";
+        public const string RouteParameter = "/pages/telephony/did";
 
         protected override string Title => "DID | Entrada";
 
@@ -45,7 +45,7 @@ namespace Sufficit.Blazor.Client.Pages.Telephony.DirectInwardDialing
             return link.TrimEnd('&');
         }
 
-        protected IEnumerable<IContact>? Providers { get; set; }
+        protected IEnumerable<IIdTitlePair>? Providers { get; set; }
 
         protected CultureInfo Culture = new CultureInfo("pt-BR");
 
@@ -140,25 +140,6 @@ namespace Sufficit.Blazor.Client.Pages.Telephony.DirectInwardDialing
                     };
                 }
             }
-
-            Providers ??= new List<IContact>()
-                {
-                    new Contact() { Id = Guid.Parse("7b0f2ebd-de91-4688-b206-a97acab03d11"), Title = "Flux Telecom"},
-                    new Contact() { Id = Guid.Parse("73d4e817-90bf-4f17-82f3-87f52115c969"), Title = "BRDID - TIP | TVN"},
-                    new Contact() { Id = Guid.Parse("01fe04e5-ad3e-43b9-b2e5-6556fd982e62"), Title = "TIP Brasil Telecom"},
-                    new Contact() { Id = Guid.Parse("68d7f117-72f7-4484-b4d8-e6cc438723b6"), Title = "America Net / DATORA"},
-                    new Contact() { Id = Guid.Parse("a80507c4-4732-493c-b084-12ec5ba37981"), Title = "Citta Telecom"},
-                    new Contact() { Id = Guid.Parse("bbdb62fe-24bf-4974-9344-be386f9369ba"), Title = "VoipLinkTronic (ALGAR)"},
-                    new Contact() { Id = Guid.Parse("2604251f-a309-4dfd-b1d2-7610cd57258f"), Title = "Transit Telecom"},
-                    new Contact() { Id = Guid.Parse("f9fc7cbb-7290-4fba-95cb-ff06bf2aafc6"), Title = "GT Group International Brasil Telecomunicações LTDA (GTGI)"},
-                    new Contact() { Id = Guid.Parse("7d6de848-e576-4564-812f-b36ac1b2ed9f"), Title = "GT Group Faturamento Individual"},
-                    new Contact() { Id = Guid.Parse("4d716201-ce36-4a8c-8123-5c620abba23f"), Title = "DirectCall"},
-                    new Contact() { Id = Guid.Parse("2dcd015b-9538-469c-9434-138ba176efa9"), Title = "Voip do Brasil"},
-                    new Contact() { Id = Guid.Parse("ba7dbdeb-a1aa-4a21-b3cc-caacd818f3ec"), Title = "Skype DID"},
-                    new Contact() { Id = Guid.Parse("ffb71292-8d68-47b6-bf54-2b57a72581bf"), Title = "Fale Sempre Mais"},
-                    new Contact() { Id = Guid.Parse("d21cfb04-9d37-473b-837c-67591a26feed"), Title = "Sufficit Soluções"},
-                    new Contact() { Id = Guid.Parse("934f107b-4008-46e2-b011-81a021e257e0"), Title = "Red Telecom"}
-                };
         }
 
         protected override async Task OnAfterRenderAsync(bool firstRender)
@@ -166,6 +147,9 @@ namespace Sufficit.Blazor.Client.Pages.Telephony.DirectInwardDialing
             await base.OnAfterRenderAsync(firstRender);
             if (!firstRender) return;
 
+            if (Providers == null)
+                Providers = await APIClient.Telephony.Carriers();
+            
             if (ObjectId != Guid.Empty)
             {
                 Item = await APIClient.Telephony.DID.ById(ObjectId);
@@ -182,14 +166,10 @@ namespace Sufficit.Blazor.Client.Pages.Telephony.DirectInwardDialing
                 // Updating view
                 await InvokeAsync(StateHasChanged);
             }
-
-            if (Providers == null)
-            {
-                // Should create an endpoint for that !
-                //Providers = await APIClient.Telephony.DID.
-            }
+      
 
             // tracking context changes
+            ContextView.OnChanged -= ContextViewChanged;
             ContextView.OnChanged += ContextViewChanged;                       
         }
 
